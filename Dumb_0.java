4e348f452f797d41c6a636674f72e6adc9ff5d28
Collin McFarland <cmcfarla@mtu.edu>
2025-02-21 11:06:37 -0500

    Dumb_0 and Dumb_1
    Dumb_0 and Dumb_1 are Java programs

---
+++ Dumb_0.java
@@ -0,0 +1,9 @@
+public class Dumb {
+    public static void main(String[] args) {
+        int i = 0;
+        while (2 + 2 != 5) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
